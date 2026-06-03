f0154e5ab552410f588b083d1c26f5431a0a799d
vvishnuweb-b <vb16vishnu@gmail.com>
2026-06-03 22:44:00 +0400

    fixed depl;oyment error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://aggregator-software.onrender.com',
         changeOrigin: true,
       },
     },
